df0dd5ef72b93df35b153cdfba1ece66ffee9fbd
jennyhedley <jennyhedley@yahoo.com>
2022-04-14 17:37:12 +1000

    set up

---
+++ bot.js
@@ -0,0 +1,41 @@
+console.log("Archive fever bot is starting");
+
+require("dotenv").config();
+const Twit = require("twit");
+const config = require("./config");
+
+const T = new Twit(config);
+
+const getTweet = require("./tweets");
+
+const stream = T.stream("statuses/filter", { follow: "1513305478897750017" });
+stream.on("tweet", reply);
+
+function reply(msg) {
+  console.log("Reply event");
+
+  const tweet = {
+    status: getTweet(),
+  };
+
+  const id = msg.id_str;
+  const name = msg.user.screen_name;
+
+  T.post(
+    "statuses/update",
+    {
+      status: "@" + name + " " + tweet.status,
+      in_reply_to_status_id: id,
+    },
+    function (err, replyMsg) {
+      if (err) {
+        if (err.message === "Tweet needs to be a bit shorter.") {
+          return reply(msg);
+        }
+        console.log(err.message);
+      } else {
+        console.log("Tweeted: " + replyMsg.text);
+      }
+    }
+  );
+}
