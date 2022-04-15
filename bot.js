ee7a4b44c23325ecd3954c75f69b0c89e4777004
jennyhedley <jennyhedley@yahoo.com>
2022-04-15 22:01:19 +1000

    express and port

--- bot.js
+++ bot.js
@@ -1,6 +1,22 @@
 console.log("Archive fever bot is starting");
-// const PORT = process.env.PORT || 80; 5000?
-bot.set("port", process.env.PORT || 80);
+
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+//For avoidong Heroku $PORT error
+app
+  .get("/", function (request, response) {
+    const result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
 
 require("dotenv").config();
 const cron = require("node-cron");
